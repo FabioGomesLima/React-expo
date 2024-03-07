69ba029f0ef61efa2bcf5b4c4d545b10a92d335d
FabioGomesLima <abioarturgomesdelima13@gmail.com>
2024-03-06 22:12:03 -0300

    Usando useState

--- App.js
+++ App.js
@@ -1,38 +1,36 @@
-import React from "react";
-import { View, Text, Image } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, Button } from 'react-native';
 
 // Função principal da aplicação
 function App() {
-  
-    return (
-        <View>
-            <Text>Olá mundo </Text>
-            <Text style={{ color:'red', fontSize:30, margin:25 }}>Meu app</Text>
-            <Text style={{ fontSize:20, color:'green' }}>
-              Gomes tv
-            </Text>
+  // Definindo estados para nome e idade
+  const [nome, setNome] = useState("Lima"); // criação de estados
+  const [idade, setIdade] = useState(22);
 
-            {/* Renderiza o componente Logo com propriedades específicas */}
-            <Logo largura={350} altura={350} funalo="Fabio Gomes"/>
-        </View>
-    );
+
+
+  // Função chamada quando o botão é pressionado para mudar o nome e a idade
+  function Entrar(nome,idade) {
+    alert('Entrou');
+    setNome('nome'); // mudar nome
+    setIdade(idade); // mudar idade
+    // Não defina estados dentro desta função
+  }
+  // parânmetros com função anônimas 
+
+  return (
+    <View style={{ marginTop: 100 }}>
+      <Button title="Mudar nome" onPress={() => Entrar('Gomes lima',33) }  />
+      <Text style={{ fontSize: 60 }}>{nome}</Text>
+      <Text style={{ fontSize: 60 }}>{idade}</Text>
+    </View>
+  );
 }
 
 // Exporta a função principal para uso externo
 export default App;
 
-// Define o componente Logo
-function Logo(props){
-    // URL da imagem
-    let img ='http://sujeitoprogramador.com/reactlogo.png';
 
-    // Retorna um componente com a imagem e o texto passados como propriedades
-    return(
-      <View>
-        <Image source={{uri:img}} style={{width:props.largura, height: props.altura}}/>
-        <Text>{props.funalo}</Text>
-      </View>
-    );
-}
+
 
 
