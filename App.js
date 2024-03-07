0257a5b32583b765cf7465f685b56bb95a3b3e40
FabioGomesLima <abioarturgomesdelima13@gmail.com>
2024-03-06 21:03:17 -0300

    Criando componentes

--- App.js
+++ App.js
@@ -1,25 +1,38 @@
 import React from "react";
 import { View, Text, Image } from 'react-native';
 
+// Função principal da aplicação
 function App() {
-    let nome ='Hungry House';
-    let img ='http://sujeitoprogramador.com/reactlogo.png';
-
+  
     return (
         <View>
-            <Text>Olá mundo</Text>
+            <Text>Olá mundo </Text>
             <Text style={{ color:'red', fontSize:30, margin:25 }}>Meu app</Text>
-            <Text style={{ fontSize:20, color:'green' }}>Gomes tv</Text>
-            <Image
-                source={{ uri: img }}
-                style={{ width: 300, height: 300 }}
-            />
-            <Text style={{ fontSize:19 }}>{nome}</Text>
+            <Text style={{ fontSize:20, color:'green' }}>
+              Gomes tv
+            </Text>
+
+            {/* Renderiza o componente Logo com propriedades específicas */}
+            <Logo largura={350} altura={350} funalo="Fabio Gomes"/>
         </View>
     );
 }
 
+// Exporta a função principal para uso externo
 export default App;
 
+// Define o componente Logo
+function Logo(props){
+    // URL da imagem
+    let img ='http://sujeitoprogramador.com/reactlogo.png';
+
+    // Retorna um componente com a imagem e o texto passados como propriedades
+    return(
+      <View>
+        <Image source={{uri:img}} style={{width:props.largura, height: props.altura}}/>
+        <Text>{props.funalo}</Text>
+      </View>
+    );
+}
 
 
