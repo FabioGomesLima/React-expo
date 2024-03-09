41bf8b63088a5080f69c0e9943c3cd3ebf4cca64
FabioGomesLima <abioarturgomesdelima13@gmail.com>
2024-03-09 18:18:16 -0300

    Trabalhando com botões

--- App.js
+++ App.js
@@ -1,25 +1,37 @@
 import React,{useState} from "react";
-import { View, Text, TextInput, StyleSheet } from "react-native";
+import { View, Text, TextInput, StyleSheet , Button} from "react-native";
 
-//Recebendo dados //#F048 
+
+
+
+//Trabalhando com botões #F049 
+
+
+
+// Definindo o componente principal 
+
 
 function App(){
     const [nome,setNome] = useState('');
+    const [input,setInput] = useState('');
 
 
-    function pegaNome(texto){
-        //alert(texto);
-        if(texto.length > 0){
-            setNome('Bem vindo'+texto);
 
-        }else{
+    function entrar(){
+        //alert('Entrou!')
 
-            setNome('');
+          // Verifica se o campo de entrada está vazio
 
-        }
-         
-       
+         if(input === ''){
+            alert('Digite seu nome!')
+            return; // Retorna se o campo estiver vazio
+
+
+            
+         }
+           setNome('Bem vindo:'+ input);
     }
+
 
 
 
@@ -28,10 +40,13 @@ function App(){
     <TextInput style={styles.input}
 
     placeholder="Digite seu nome aqui"
-    onChangeText={(text)=> pegaNome(text)}
+    onChangeText={(texto)=>setInput(texto)} // pegar o texto que digitamos e colocar dentro useState
     
     
     />
+    {/* Botão para entrar, chama a função 'entrar' quando pressionado */}
+    <Button title="Entrar" onPress={entrar}/> 
+
     <Text style={styles.texto}>{nome}</Text>
 
 
@@ -42,20 +57,25 @@ function App(){
 
 const styles = StyleSheet.create({
    container:{
-      flex:1
+      flex:1,
+   
+   
    },
    input:{
     height:50,
     borderWidth:1,
     margin:10, // tratar dos espaçamentos dos lados
-    padding:17, // tratar da parte interna do campo 
+    padding:10, // tratar da parte interna do campo 
     fontSize: 20,
-    
+    marginTop:80, // Adicionando margem superior para descer o campo de entrada
+
+   
    
    },
    texto:{
     textAlign:'center',
     fontSize:25,
+    marginTop:19,
     
    }
 });
