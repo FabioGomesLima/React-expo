2f3a7f2b6a5bd3c5897489717a2078c247fcd6d0
FabioGomesLima <abioarturgomesdelima13@gmail.com>
2024-03-09 17:35:25 -0300

    Recebendo dados

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
-import React from "react";      
-import { View, Text,  } from "react-native";
+import React,{useState} from "react";
+import { View, Text, TextInput, StyleSheet } from "react-native";
+
+//Recebendo dados //#F048 
 
 function App(){
+    const [nome,setNome] = useState('');
+
+
+    function pegaNome(texto){
+        //alert(texto);
+        if(texto.length > 0){
+            setNome('Bem vindo'+texto);
+
+        }else{
+
+            setNome('');
+
+        }
+         
+       
+    }
+
+
+
     return(
-        <View style={{flex:1, 
-        flexDirection:'row' , 
-        alignItems:'center', 
-        justifyContent:'center', 
+<View style={styles.container}>     
+    <TextInput style={styles.input}
 
-         }}>
+    placeholder="Digite seu nome aqui"
+    onChangeText={(text)=> pegaNome(text)}
+    
+    
+    />
+    <Text style={styles.texto}>{nome}</Text>
 
-            <View style={{height:90, width:90, backgroundColor:'#121212'}}></View>
-            <View style={{height:90, width:90, backgroundColor:'red'}}></View>
-            <View style={{height:90, width:90, backgroundColor:'green'}}></View>
 
-        </View>
+
+ </View>
     );
 }
+
+const styles = StyleSheet.create({
+   container:{
+      flex:1
+   },
+   input:{
+    height:50,
+    borderWidth:1,
+    margin:10, // tratar dos espaçamentos dos lados
+    padding:17, // tratar da parte interna do campo 
+    fontSize: 20,
+    
+   
+   },
+   texto:{
+    textAlign:'center',
+    fontSize:25,
+    
+   }
+});
 
 export default App;
